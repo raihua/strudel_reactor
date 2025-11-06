eb014474af8aa812c467eac15a050ac3de8aaea9
raihua <raihua.dev@gmail.com>
2025-11-07 02:28:45 +1030

    Fix SongSelectors use of strudel refs

--- src/components/SongSelector.js
+++ src/components/SongSelector.js
@@ -3,17 +3,19 @@ import { stranger_tune } from "../tunes";
 
 export default function SongSelector({ globalEditorRef, strudelCodeRef }) {
   const songsList = useRef(new Map([["stranger_tune", stranger_tune]]))
-  const [selectedSong, setSelectedSong] = useState([]);
+  const [selectedSong, setSelectedSong] = useState("stranger_tune");
 
   useEffect(() => {
-    console.log(songsList.current.get("stranger_tune"));
+    let songCode = songsList.current?.get(selectedSong);
+    strudelCodeRef.current = songCode;
+    globalEditorRef.current?.setCode(songCode);
   }, [selectedSong])
 
 
   return (
     <>
     <p>Selected song: {selectedSong}</p>
-    <select class="form-select" multiple aria-label="multiple select example" onChange={(e) => setSelectedSong(e.target.value)}>
+    <select className="form-select" multiple aria-label="multiple select example" onChange={(e) => setSelectedSong(e.target.value)}>
       {songsList ? (
         songsList.current.entries().map(([songName, songCode]) => <option value={[songName]}>{songName}</option>)
       ) : (
