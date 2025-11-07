71411d1fa08e43103992995a013b555ef91bd710
raihua <raihua.dev@gmail.com>
2025-11-07 15:28:58 +1030

    Add add song mode

--- src/components/SongSelector.js
+++ src/components/SongSelector.js
@@ -5,6 +5,8 @@ export default function SongSelector({ globalEditorRef, strudelCodeRef }) {
   const songsList = useRef(new Map([["stranger_tune", stranger_tune]]))
   const [selectedSong, setSelectedSong] = useState("stranger_tune");
   const [addSongMode, setAddSongMode] = useState(false);
+  const newSongCode = useRef("");
+  const newSongName = useRef("");
 
   useEffect(() => {
     let songCode = songsList.current?.get(selectedSong);
@@ -12,25 +14,30 @@ export default function SongSelector({ globalEditorRef, strudelCodeRef }) {
     globalEditorRef.current?.setCode(songCode);
   }, [selectedSong])
 
+  function saveNewSong() {
+    songsList.current.set(newSongName.current, newSongCode.current);
+    console.log("saved " + newSongName.current);
+  }
+
 
   return (
     <>
     <p>Selected song: {selectedSong}</p>
-    <select className="form-select" multiple aria-label="multiple select example" onChange={(e) => setSelectedSong(e.target.value)}>
+    <select className="form-select" onChange={(e) => setSelectedSong(e.target.value)}>
       {songsList ? (
         songsList.current.entries().map(([songName, songCode]) => <option value={[songName]}>{songName}</option>)
       ) : (
         <></>
       )}
     </select>
-    <button className="btn btn-outline-primary" onClick={() => setAddSongMode(!addSongMode)}>{addSongMode ? "Close add song mode" : "Open add song mode"}</button>
+    <button className="btn btn-outline-primary" onClick={() => setAddSongMode(!addSongMode)}>{addSongMode ? "Close and load add song mod" : "Open add song mode"}</button>
     {addSongMode ? <div className="row">
       <div className="col">
       <label>New song name:</label>
-      <input type="text" className="form-control"></input>
+      <input type="text" className="form-control" onChange={(e) => newSongName.current = e.target.value}></input>
       <label>Strudel song code:</label>
-      <textarea className="form-control"></textarea>
-      <button className="btn btn-outline-success">Save</button>
+      <textarea className="form-control" onChange={(e) => newSongCode.current = e.target.value}></textarea>
+      <button className="btn btn-outline-success" onClick={() => saveNewSong()}>Save</button>
       </div>
     </div> : ""}
     </>
