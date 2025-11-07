11c57c8218e371ca0e85b0e5d860b41a7631ca7d
raihua <raihua.dev@gmail.com>
2025-11-07 15:11:17 +1030

    Add add button for song mode

--- src/components/SongSelector.js
+++ src/components/SongSelector.js
@@ -4,6 +4,7 @@ import { stranger_tune } from "../tunes";
 export default function SongSelector({ globalEditorRef, strudelCodeRef }) {
   const songsList = useRef(new Map([["stranger_tune", stranger_tune]]))
   const [selectedSong, setSelectedSong] = useState("stranger_tune");
+  const [addSongMode, setAddSongMode] = useState(false);
 
   useEffect(() => {
     let songCode = songsList.current?.get(selectedSong);
@@ -22,6 +23,16 @@ export default function SongSelector({ globalEditorRef, strudelCodeRef }) {
         <></>
       )}
     </select>
+    <button className="btn btn-outline-primary" onClick={() => setAddSongMode(!addSongMode)}>{addSongMode ? "Close add song mode" : "Open add song mode"}</button>
+    {addSongMode ? <div className="row">
+      <div className="col">
+      <label>New song name:</label>
+      <input type="text" className="form-control"></input>
+      <label>Strudel song code:</label>
+      <textarea className="form-control"></textarea>
+      <button className="btn btn-outline-success">Save</button>
+      </div>
+    </div> : ""}
     </>
   );
 }
