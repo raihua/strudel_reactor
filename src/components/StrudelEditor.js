45f4d93a08ee4912b5671a03bc6efa8f53d87f82
raihua <raihua.dev@gmail.com>
2025-11-18 05:09:16 +1030

    Change TextPreprocessor to StrudelEditor
    This improve clarity with purpose

---
+++ src/components/StrudelEditor.js
@@ -0,0 +1,23 @@
+import { stranger_tune } from "../tunes";
+import { ProcessStudelCode } from "../utils/preprocessors";
+import { useEffect, useState } from "react";
+
+export default function StrudelEditor({
+  globalEditorRef,
+  strudelCode,
+  setStrudelCode,
+}) {
+  return (
+    <div>
+      <label htmlFor="exampleFormControlTextarea1" className="form-label">
+        Strudel Code Editor:
+      </label>
+      <textarea
+        className="form-control"
+        rows="15"
+        value={strudelCode}
+        onChange={(e) => setStrudelCode(e.target.value)}
+      ></textarea>
+    </div>
+  );
+}
